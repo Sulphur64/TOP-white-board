bc407347969651060198211a1f6b1c14d768e813
Sulphur64 <128920405+Sulphur64@users.noreply.github.com>
2023-05-01 18:07:13 +0200

    finished UI, tilesystem() and shadesdraw()
    to do:
    
    rainbow. test.

--- board.js
+++ board.js
@@ -5,9 +5,15 @@ const startButton = document.createElement('button');
 const mainFrame = document.createElement('div');
     
     const drawMenu = document.createElement('div');
+        const menuInputText = document.createElement('div');
+        const menuInputMatrix = document.createElement('input');
+        const menuVerbose = document.createElement('div');
+        const menuModshades = document.createElement('button');
+        const menuModRainbow = document.createElement('button');
     
     const drawFrame = document.createElement('div');
-        const tiles = document.createElement('div');
+        
+
 
 eventHandler(0);
 
@@ -49,9 +55,68 @@ function startBoard (state) { // handles the mainframepop and reset
         mainFrame.appendChild(drawFrame);
         mainFrame.appendChild(drawMenu);
         
-        drawMenu.classList.add('draw-menu')
-        drawFrame.classList.add('draw-frame')
+        drawMenu.classList.add('draw-menu');
+        drawFrame.classList.add('draw-frame');
 
+        drawMenu.appendChild(menuInputText);
+        menuInputText.classList.add('draw-menu-text');
+        menuInputText.textContent = "change matrix resolution:";
+
+        drawMenu.appendChild(menuInputMatrix);
+        menuInputMatrix.placeholder = "number between 1 and 100";
+
+        gridFiller(16);
+        menuInputMatrix.addEventListener("change",()=>gridFiller(menuInputMatrix.value)) 
+
+        drawMenu.appendChild(menuVerbose);
+        menuVerbose.classList.add('draw-menu-text');
+        menuVerbose.textContent = "drawing style:";
+
+        drawMenu.appendChild(menuModshades);
+        menuModshades.classList.add('draw-menu-button');
+        menuModshades.classList.add('hel');
+        menuModshades.textContent = "Hel";
+        menuModshades.addEventListener("click", ()=> menuDrawStyleShades());
+        
+        drawMenu.appendChild(menuModRainbow);
+        menuModRainbow.classList.add('draw-menu-button');
+        menuModRainbow.classList.add('bifrost');
+        menuModRainbow.textContent = "bifrost";
         
     };
-}
+};
+
+
+function menuDrawStyleShades(){ //draw in shades of grey
+    const tiles = document.querySelectorAll('div.tiles');
+    tiles.forEach(node => {
+        node.addEventListener('mouseenter',()=>{
+            console.log(node.style.backgroundColor)
+            let RGBValues = node.style.backgroundColor.replaceAll(/[^0-9,]/ig,"").split(",");
+            //RGBValues.forEach(value => value -= 10);
+            node.style.backgroundColor = `rgb(${RGBValues[0]-10}, ${RGBValues[1]-10}, ${RGBValues[2]-10})`;
+        });
+    });
+};
+
+function menuDrawStyleRainbow(){ // draw in rainbow
+
+};
+
+function gridFiller(num){ //build the cells and attach an even listener to them
+    if((num > 0) && (num <= 100)){
+            while (drawFrame.firstChild){
+            drawFrame.removeChild(drawFrame.lastChild);
+        };
+
+        for (let i=0;i < (num * num);i++){
+            
+            drawFrame.setAttribute('style',`grid-template: repeat( ${num}, 1fr) / repeat(${num}, 1fr)`)
+            const tile= document.createElement("div");
+            tile.classList.add("tiles");
+            tile.style.backgroundColor = 'rgb(255, 255, 255)';
+            drawFrame.appendChild(tile);
+
+        };
+    };
+};
